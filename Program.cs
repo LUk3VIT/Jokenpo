9489eda85d05e4e15e078f351ae4c7a86ccf7e69
Lucas Dias <91563939+LUk3VIT@users.noreply.github.com>
2026-02-05 19:44:58 -0300

    inicial

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿string jogadorUm;
+int jogadorUmPonto = 0;
+
+string jogadorDois;
+int jogadorDoisPonto = 0;
+
+string opcao = "inicio";
+
+while (opcao == "inicio")
+{
+    Console.WriteLine("Jogador 1 escolha entre: \nPedra - Papel - Tesoura");
+    jogadorUm = Console.ReadLine();
+
+    Console.Clear();
+
+    Console.WriteLine("Jogador 2 escolha entre: \nPedra - Papel - Tesoura");
+    jogadorDois= Console.ReadLine();
+
+    Console.Clear();
+
+    if (jogadorUm == jogadorDois)
+    {
+        Console.WriteLine("Empate!");
+    } 
+    else if ((jogadorUm == "Pedra" && jogadorDois == "Tesoura") || (jogadorUm == "Papel" && jogadorDois == "Pedra") || (jogadorUm == "Tesoura" && jogadorDois == "Papel"))
+    {
+        jogadorUmPonto =+ 1;
+        Console.WriteLine("Jogador 1 ganhou um ponto!");
+    } else {
+        jogadorDoisPonto =+ 1;
+        Console.WriteLine("Jogador 2 ganhou um ponto! \n\n");
+    }
+
+    Console.WriteLine("Placar\n\n");
+    Console.WriteLine($"Pontos do jogador Um: {jogadorUmPonto}\n");
+    Console.WriteLine($"Pontos do jogodor Dois: {jogadorDoisPonto}\n\n");
+
+    Console.WriteLine("Quer jogar mais uma: \n (1) sim  | (2) não");
+    opcao = Console.ReadLine();
+
+    if (opcao == "2"){
+        opcao = "Fim";
+    } else {
+        opcao = "inicio";
+    }
+}
